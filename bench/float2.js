15df3f67b6b79d5964249d1cf1e9273176d53ac7
Pierre Curto <pierre.curto@gmail.com>
2012-05-28 09:01:19 +0200

    help debug float() 0.3.0 slowness

--- bench/float2.js
+++ bench/float2.js
@@ -1,13 +1,14 @@
 var numString = '123.456 '
 
 var data= ''
-for (var i = 0; i < 100000; i++)
+for (var i = 0; i < 2; i++)
 	data += numString
 
 function floatListParser () {
 	atok.float()
 		.ignore(true)
 		.addRule(1, 'space')
+		.on('data', console.log)
 }
 
 var Parser = require('..').createParser(floatListParser)
@@ -17,7 +18,8 @@ console.time('split')
 data.split(' ').forEach(Number)
 console.timeEnd('split')
 
-// p.atok.on('data', console.log)
+p.debug(true)
+p.on('debug', console.log)
 console.time('parser')
 p.write(data)
 console.timeEnd('parser')
